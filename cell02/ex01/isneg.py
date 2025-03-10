feda5ce50429350ac08b41a2242cf3d06e0f274d
KAYASANGCOMB <40341@prot.ac.th>
2025-03-10 13:41:33 +0700

    Am I Negative?

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+number = float(input())
+if number > 0 : print("This number is positive.")
+elif number < 0 : print("This number is negative.")
+elif number == 0 : print("This number is both positive and negative.")
