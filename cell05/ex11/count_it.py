636394711c00757f07589e440f815bc0b4ddf062
KAYASANGCOMB <40341@prot.ac.th>
2025-03-13 12:22:44 +0700

    ccount it

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,11 @@
+import sys
+x = input('Enter here (separated by quotes): ')
+y = [item.strip() for item in x.split('"') if item.strip()]
+y = [a for a in y]
+sys.argv.extend(y)
+if len(sys.argv) == 1:
+    print("none")
+else:
+    print(f"parameters: {len(sys.argv) - 1}")
+    for y in sys.argv[1:]:
+        print(f"{y} {len(y)}")
