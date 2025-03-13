f0e12fac90b7f6ec9784200473c10080c318900c
KAYASANGCOMB <40341@prot.ac.th>
2025-03-13 09:17:03 +0700

    age

---
+++ cell04/ex01/age.py
@@ -0,0 +1,2 @@
+age = float(input("Enter your age: "))
+print(f"You are currently {age} years old.\nIn 10 years. you'll be {age + 10} years old.\nIn 20 years. you'll be {age + 20} years old.\nIn 30 years. you'll be {age + 30} years old.")
