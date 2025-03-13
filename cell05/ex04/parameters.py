440a6e773dd025d07babb6a8fd8fe2367c47edc2
KAYASANGCOMB <40341@prot.ac.th>
2025-03-13 11:25:38 +0700

    Update parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,2 +1,7 @@
-arr1 = [2, 8, 9, 48, 8, 22, -12, 2]
-print(f'Number of parameters:', len(y))
+import sys
+x = input('Enter here (separated by spaces and quotes): ')
+y = x.split('"')
+y = [y.strip() for y in y if y.strip()]
+sys.argv.extend(y)
+print("Arguments:", y)
+print(f'Number of parameters: {len(sys.argv) - 1}')
